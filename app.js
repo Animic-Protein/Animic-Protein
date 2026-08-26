6f24dd9df62ebc596586acb5a9bf49f97b8ef9b1
Zardoz <mytcmv6kgs@privaterelay.appleid.com>
2026-08-26 16:11:22 +0200

    Publica el Còdex canònic amb Pages i mode instal·lable

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ coreScript.onload=()=>{
   document.body.appendChild(germScript);
 };
 document.body.appendChild(coreScript);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js'));
+}
